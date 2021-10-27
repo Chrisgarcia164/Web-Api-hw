4de4660642a872404ae6bea556b9c179eaf8d520
Christopher Garcia <chrisgarcia0903@gmail.com>
2021-10-26 21:52:33 -0400

    saves and displays what the user has to do

--- script.js
+++ script.js
@@ -19,4 +19,18 @@
       var blockHour = $(this).parent().attr('time')
       var todo = $(this).siblings('.description').val()
       localStorage.setItem(blockHour, todo)
+      addTodos()
     })
+    function addTodos() {
+      $('#t9 .description').val(localStorage.getItem('9'));
+      $('#t10 .description').val(localStorage.getItem('10'));
+      $('#t11 .description').val(localStorage.getItem('11'));
+      $('#t12 .description').val(localStorage.getItem('12'));
+      $('#t13 .description').val(localStorage.getItem('13'));
+      $('#t14 .description').val(localStorage.getItem('14'));
+      $('#t15 .description').val(localStorage.getItem('15'));
+      $('#t16 .description').val(localStorage.getItem('16'));
+      $('#t17 .description').val(localStorage.getItem('17'));
+
+    }
+    addTodos()
